77d9cf8d9497ae2676bbd1ae77e46397f5dc61e2
Lucas <lucasgeansantos@gmail.com>
2024-10-30 12:02:32 -0300

    receber porta da heroku

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
-const cors = require('cors'); // Importar o pacote cors
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Usar o middleware CORS
-app.use(cors()); // Isso permite todas as origens
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
     
-    // Quando o servidor receber a localização
     socket.on('location', (data) => {
-        console.log('received location:', data);
-        
-        // Enviar a localização recebida de volta para todos os clientes conectados
-        io.emit('location', data); // isso envia para todos, incluindo o remetente
+        io.emit('location', data);
     });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
 });
 
 app.get('/', (req, res) => {
-    console.log('Hello World');
     res.send('Hello World');
 });
 
-server.listen(3000, () => {
+server.listen(PORT , () => {
     console.log('listening on *:3000');
 });
